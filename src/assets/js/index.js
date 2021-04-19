03f642b2601a4ed09b01a3582a1827194e5b3bba
lorenzo cadente <lorenzo.cadente@accenture.com>
2021-04-19 18:35:51 +0200

    email

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,10 +5,9 @@ import Header from "../../components/header/header";
 import Gallery from "../../components/gallery/gallery";
 import ModalVideo from "../../components/modals/modal";
 import VideoPage from "./videoPage";
+import Mail from "./mail";
 
 window.setInterval(function() {
-  console.log('sono passati');
-  
   window.localStorage.removeItem('videos');
 }, 600000);
 
@@ -17,5 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
   try { new Gallery().load() } catch (err) { console.error(err) }
   try { new ModalVideo().load() } catch (err) { console.error(err) }
   try { new VideoPage().load() } catch (err) { console.error(err) }
+  try { new Mail().load() } catch (err) { console.error(err) }
 });
 
