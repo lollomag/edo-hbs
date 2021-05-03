4ef334361571465c03cee88acdaf9ef7eb31e4ea
lorenzo cadente <lorenzo.cadente@accenture.com>
2021-05-03 18:27:23 +0200

    upd

--- src/components/modals/modal.js
+++ src/components/modals/modal.js
@@ -3,17 +3,47 @@ export default class Modals {
     this.init();
   }
   init() {
+    const videoList = [
+      {
+        id: "0",
+        vimeoId: "535609734",
+        title: "Il titolo dai",
+        description: "la descrizione migliore"
+      },
+      {
+        id: "1",
+        vimeoId: "474122376",
+        title: "Il titolo dai 2",
+        description: "la descrizione migliore 2"
+      },
+      {
+        id: "2",
+        vimeoId: "500386215",
+        title: "Il titolo dai 3",
+        description: "la descrizione migliore 3"
+      },
+      {
+        id: "3",
+        vimeoId: "429554240",
+        title: "Il titolo dai 4",
+        description: "la descrizione migliore 4"
+      }
+    ];
+
+
     $('#modal-video').on('show.bs.modal', function (e) {
-      const videoSets = JSON.parse(localStorage.getItem('videos'));
       const id = e.relatedTarget.getAttribute('data-id');
-      const video = videoSets.find(el => el.id == id);
+      const video = videoList.find(el => el.id == id);
 
       const wrapper = document.querySelector('.modal-video .modal-body');
         wrapper.insertAdjacentHTML('afterbegin', `
         <h5 class="title">${video.title}</h5>
-        <p class="description">${video.description}</p>
-        <video class="mt-50" src="http://localhost:1337${video.cover.url}" controls controlsList="nodownload"></video>
+        <p class="description mb-50">${video.description}</p>
+        <div id="player-modal" data-plyr-provider="vimeo" data-plyr-embed-id="${video.vimeoId}"></div>
           `)
+
+      const videoModal = wrapper.querySelector('#player-modal')
+      const initializeVideo = new Plyr(videoModal);
     })
 
     $('#modal-video').on('hidden.bs.modal', function (e) {
