ada55f4ee97e8ff0b04361dc6dec39f07a04ad4a
lorenzo cadente <lorenzo.cadente@accenture.com>
2021-05-03 11:18:53 +0200

    fix foto

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,18 +2,14 @@ import "./polyfill";
 import "./lightgallery";
 
 import Header from "../../components/header/header";
-// import Gallery from "../../components/gallery/gallery";
+import Gallery from "../../components/gallery/gallery";
 import ModalVideo from "../../components/modals/modal";
 // import VideoPage from "./videoPage";
 import Mail from "./mail";
 
-// window.setInterval(function() {
-//   window.localStorage.removeItem('videos');
-// }, 600000);
-
 document.addEventListener('DOMContentLoaded', () => {
   try { new Header().load() } catch (err) { console.error(err) }
-  // try { new Gallery().load() } catch (err) { console.error(err) }
+  try { new Gallery().load() } catch (err) { console.error(err) }
   try { new ModalVideo().load() } catch (err) { console.error(err) }
   // try { new VideoPage().load() } catch (err) { console.error(err) }
   try { new Mail().load() } catch (err) { console.error(err) }
