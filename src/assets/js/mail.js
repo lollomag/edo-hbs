b9ea50a69f44eacda8f645cc4d9e7e9f70e5966a
lorenzo cadente <lorenzo.cadente@accenture.com>
2021-05-11 17:42:47 +0200

    validation

--- src/assets/js/mail.js
+++ src/assets/js/mail.js
@@ -1,5 +1,4 @@
 import emailjs from 'emailjs-com';
-// import { validateForm } from "./validate-form";
 
 export function sendEmail() {
   emailjs.init("user_z3RyQJDmg35SxTcFSRJiO");
@@ -20,93 +19,27 @@ export function sendEmail() {
     email
   };
 
-  console.log('arriva nellinvio', templateParams);
-
-
   btn.classList.add('loading')
   const serviceID = 'service_h9shcxh'
-  // const templateID = "template_fliqvno";
-  const templateID = "template_ix52lcj"
+  const templateID = "template_fliqvno";
 
   emailjs.send(serviceID, templateID, templateParams)
     .then((response) => {
 
       if (response.status === 200) {
-        console.log('arriva nellinvio');
-
         $('#modal-email').modal('show')
         btn.classList.remove('loading');
-        this.cleanForm()
+        cleanForm()
       }
     }, (error) => { });
 }
 
-// export default class Mail {
-//   load() {
-//     this.init();
-//   }
-//   init() {
-//     const sendBtn = document.querySelector('#form-send-button');
 
-//     if (sendBtn) {
-//       sendBtn.addEventListener('click', () => {
-//         console.log(validateForm());
+function cleanForm() {
+  const form = document.querySelector('#info-form')
 
-//         // if (!validateForm()) return
-//         // console.log('click');
-
-//         // this.mail();
-//       })
-//     }
-
-//   }
-
-//   mail() {
-//     emailjs.init("user_z3RyQJDmg35SxTcFSRJiO");
-
-//     const btn = document.querySelector('#form-send-button');
-
-//     const form = document.querySelector('#info-form');
-
-//     const name = form.querySelector('#name').value;
-//     const email = form.querySelector('#email').value;
-//     const object = form.querySelector('#object').value;
-//     const message = form.querySelector('#message').value;
-
-//     let templateParams = {
-//       object,
-//       name,
-//       message,
-//       email
-//     };
-
-//     console.log('arriva nellinvio', templateParams);
-
-
-//     btn.classList.add('loading')
-//     const serviceID = 'service_h9shcxh'
-//     // const templateID = "template_fliqvno";
-//     const templateID = "template_ix52lcj"
-
-//     emailjs.send(serviceID, templateID, templateParams)
-//       .then((response) => {
-
-//         if (response.status === 200) {
-//           console.log('arriva nellinvio');
-
-//           $('#modal-email').modal('show')
-//           btn.classList.remove('loading');
-//           this.cleanForm()
-//         }
-//       }, (error) => { });
-//   }
-
-//   cleanForm() {
-//     const form = document.querySelector('#info-form');
-
-//     form.querySelector('#name').value = ""
-//     form.querySelector('#email').value = ""
-//     form.querySelector('#object').value = ""
-//     form.querySelector('#message').value = ""
-//   }
-// }
+  form.querySelector('#name').value = ""
+  form.querySelector('#email').value = ""
+  form.querySelector('#object').value = ""
+  form.querySelector('#message').value = ""
+}
--- src/assets/js/validate-form.js
+++ src/assets/js/validate-form.js
@@ -1,28 +1,17 @@
 import { sendEmail } from "./mail";
 
-
 const sendBtn = document.querySelector('#form-send-button');
 
 if (sendBtn) {
   sendBtn.addEventListener('click', () => {
-    console.log('daje');
-    validateForm()
+    if(!validateForm()) return
+    sendEmail()
   })
 }
 
 function validateForm() {
-  // let x= false
-  $('#info-form').parsley()
-    // .on('form:validated', function () {
-    //   console.log('entra al validate', this);
-    //   if (this.validationResult) {
-    //     x = true
-    //   }
-    // })
-    .on('form:success', function () {
-      // console.log('non ci arriva qua si');
-      return false; // Don't submit form for this demo
-      // console.log('non ci arriva qua');
-
-    });
+  const form = $('#info-form').parsley();
+  
+  if(!form.validate()) return false
+  return true
 }
