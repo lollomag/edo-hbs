454e562f46572683dd71976bf562a005ad9aad68
lorenzo cadente <lorenzo.cadente@accenture.com>
2021-05-23 17:27:50 +0200

    update

--- src/components/bio-carousel/bio-carousel.js
+++ src/components/bio-carousel/bio-carousel.js
@@ -1,24 +1,24 @@
 $('.bio-carousel').slick({
   infinite: true,
-  slidesToShow: 3,
+  slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
   arrows: false,
-  responsive: [
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 520,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+  // responsive: [
+  //   {
+  //     breakpoint: 1280,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 2
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 520,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
+  // ]
 });
